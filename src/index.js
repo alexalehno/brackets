2e347520a6a3d2b7acfb901ae00251850b767dc0
alexalehno <alekswakeup@gmail.com>
2021-08-06 13:39:11 +0300

    soleve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, config) {
+  let brackets = str;
+  let count = 0;
+
+  while (brackets) {
+    for (let i = 0; i < config.length; i++) {
+      brackets = brackets.split(config[i].join("")).join("");
+    }
+
+    count++;
+    if (count > str.length / 2) return false;
+  }
+
+  return true;
+};
